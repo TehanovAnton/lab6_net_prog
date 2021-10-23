65f89da5e8992256b835c80b848bbb6162e852b0
antony <tehanovanton@gmail.com>
2021-10-23 23:18:03 +0300

    server recives all clients

--- Lab6/ServerNP.cpp
+++ Lab6/ServerNP.cpp
@@ -9,22 +9,27 @@ int main()
 		if ((hPipe = CreateNamedPipe(pipeName, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE|PIPE_WAIT, 1, NULL, NULL, INFINITE, NULL)) == INVALID_HANDLE_VALUE)
 			throw ERROR_CREATENAMEDPIPE;
 
-		if (!ConnectNamedPipe(hPipe, NULL))
-			throw ERROR_CONNECTNAMEDPIPE;
-
-		cout << "connected" << endl;
-
 		char inBuf[BUF_SIZE];
 		DWORD dwBytesRead = 0;
-		if (!ReadFile(hPipe, inBuf, sizeof(inBuf), &dwBytesRead, NULL))
-			throw ERROR_READFILE;
+		while (true) {
+			if (!ConnectNamedPipe(hPipe, NULL))
+				throw ERROR_CONNECTNAMEDPIPE;
 
-		cout << "Client message: " << inBuf << endl;
+			cout << "connected" << endl;
 
-		char outBuf[BUF_SIZE] = "hello client";
-		DWORD dwBytesWritten = 0;
-		if (!WriteFile(hPipe, outBuf, sizeof(outBuf), &dwBytesWritten, NULL))
-			throw ERROR_READFILE;		
+			if (!ReadFile(hPipe, inBuf, sizeof(inBuf), &dwBytesRead, NULL))
+				throw ERROR_READFILE;
+
+			cout << "Client message: " << inBuf << endl;
+
+			char outBuf[BUF_SIZE] = "hello client";
+			DWORD dwBytesWritten = 0;
+			if (!WriteFile(hPipe, outBuf, sizeof(outBuf), &dwBytesWritten, NULL))
+				throw ERROR_READFILE;
+
+			DisconnectNamedPipe(hPipe);		cout << "disconnected\n" << endl;
+			memset(inBuf, 0, sizeof(inBuf));	memset(outBuf, 0, sizeof(outBuf));
+		}
 
 		CloseHandle(hPipe);
 	}
