cf67f759f4a1a3985d94bb18e2f1c837ba0d30c2
antony <tehanovanton@gmail.com>
2021-10-23 13:02:22 +0300

    Client with TransactNamedPipe

--- ClientNP/ClientNP.cpp
+++ ClientNP/ClientNP.cpp
@@ -4,15 +4,24 @@
 int main()
 {
 	try
-	{		
+	{
+		LPCWSTR pipeName = TEXT(PIPE_NAME);		
+		HANDLE hPipe;
+		if ((hPipe = CreateFile(pipeName, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL)) == INVALID_HANDLE_VALUE)
+			throw ERROR_CREATEFILE;
+
+		DWORD dwMode = PIPE_READMODE_MESSAGE;
+		SetNamedPipeHandleState(hPipe, &dwMode, NULL, NULL);  
+
 		char inBuff[BUF_SIZE],
 			outBuff[BUF_SIZE] = "hello server";
 		DWORD nBytesRead;
-		LPCWSTR pipeName = TEXT(PIPE_NAME);
-		if (!CallNamedPipe(pipeName, outBuff, sizeof(outBuff), inBuff, sizeof(inBuff), &nBytesRead, NMPWAIT_WAIT_FOREVER))
-			throw ERROR_CALLNAMEDPIPE;
+		if (!TransactNamedPipe(hPipe, outBuff, sizeof(inBuff), inBuff, sizeof(inBuff), &nBytesRead, NULL))
+			throw ERROR_TRANSACTNAMEDPIPE;
 
 		cout << "Server response: " << inBuff << endl;
+
+		CloseHandle(hPipe);
 	}
 	catch (const char* err)
 	{
